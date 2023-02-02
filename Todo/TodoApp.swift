96b01618843776d168fdc7a30f4ec9fe6b32c83e
Freeythm <freeythm@gmail.com>
2023-02-02 19:45:46 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 中西弘海 on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
